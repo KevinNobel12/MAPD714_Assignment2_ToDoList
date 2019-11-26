5cc9c3a282b1d86efc4a11b058b64aaf9f8f0585
Kevin Nobel <kev17nobel@gmail.com>
2019-11-26 13:06:49 -0500

    Initial Commit

---
+++ MAPD714_KevinNobel_Assignment2_ToDoList_Final/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAPD714_KevinNobel_Assignment2_ToDoList_Final
+//
+//  Created by Kevin Nobel on 2019-11-26.
+//  Copyright © 2019 Kevin Nobel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
